4a223213744901bb5442028c6b7b5fff9c6220bd
Łukasz Spasiński <01171680@pw.edu.pl>
2024-11-03 20:28:30 +0100

    zejsciowka laby2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,38 @@
 #pragma once
 
 #include "Resource.hpp"
+#include <memory> // Dla std::unique_ptr
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    // Wskaünik do zasobu
+    std::unique_ptr< Resource > zasob;
+
+public:
+    // Konstruktor domyúlny
+    ResourceManager() : zasob(std::make_unique< Resource >()) {}
+
+    // Konstruktor kopiujπcy
+    ResourceManager(const ResourceManager& other)
+        : zasob(other.zasob ? std::make_unique< Resource >(*other.zasob) : nullptr)
+    {}
+
+    // Operator przypisania kopiujπcy
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) {
+            zasob = other.zasob ? std::make_unique< Resource >(*other.zasob) : nullptr;
+        }
+        return *this;
+    }
+
+    // Destruktor
+    ~ResourceManager() = default;
+
+    // Metoda pobierz - zwraca wynik zawo≥ania metody get obiektu Resource
+    // Sprawdza, czy zasÛb istnieje
+    double pobierz() { 
+        return zasob ? zasob->get() : 0.0; 
+    }
 };
